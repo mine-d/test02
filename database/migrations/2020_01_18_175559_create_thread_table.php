314e50b00f402e6fa028b97cb663d36cfdf2fad4
mine <ykmndisk@gmail.com>
2020-01-23 01:11:11 +0900

    first commit

---
+++ database/migrations/2020_01_18_175559_create_thread_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thread', function (Blueprint $table) {
+            $table->bigIncrements('thread_id');
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('thread');
+    }
+}
---
+++ database/migrations/2020_01_18_175824_create_res_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('res', function (Blueprint $table) {
+            $table->bigIncrements('res_id');
+            $table->bigInteger('thread_id')->unsigned();
+            $table->string('name')->nullable();
+            $table->timestamps();
+            $table->foreign('thread_id')->references('thread_id')->on('thread');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('res');
+    }
+}
